038f2086877dac302af5401b0789c0420ae74756
LaisFSGomes <laisgomes@alu.ufc.br>
2023-01-03 18:54:19 -0300

    chore: trabalhando com property Binding no componente create-thought

--- src/app/components/thoughts/create-thought/create-thought.component.ts
+++ src/app/components/thoughts/create-thought/create-thought.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-thought',
   templateUrl: './create-thought.component.html',
-  styleUrls: ['./create-thought.component.css']
+  styleUrls: ['./create-thought.component.css'],
 })
 export class CreateThoughtComponent {
-
+  thought = {
+    id: "1",
+    content: "On my machine, it works!",
+    author: "Developer",
+    model: "",
+  }
+  constructor() {}
+  ngOnInit(): void {}
 }
