78ce34b6dc985398b0f0f616e45eedef4d93955c
LaisFSGomes <laisgomes@alu.ufc.br>
2023-01-04 11:04:38 -0300

    chore: trabalhando com as diretivas ngFor, *ngIf e ngClass

--- src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
+++ src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-list-thoughts',
-  templateUrl: './list-thoughts.component.html',
-  styleUrls: ['./list-thoughts.component.css']
+  selector: "app-list-thoughts",
+  templateUrl: "./list-thoughts.component.html",
+  styleUrls: ["./list-thoughts.component.css"],
 })
 export class ListThoughtsComponent {
   thoughtsList = [
@@ -18,9 +18,28 @@ export class ListThoughtsComponent {
       model: "modelo1",
     },
     {
-      content: "I can't know everything",
-      author: "Developer",
+      content: "hello world",
+      author: "Dev",
+      model: "modelo1",
+    },
+    {
+      content: `The Beatles were an English rock band, formed in Liverpool in 1960,
+      that comprised John Lennon, Paul McCartney, George Harrison and Ringo Starr.
+      They are regarded as the most influential band of all time[1] and were integral
+      to the development of 1960s counterculture and popular music's recognition as an
+      art form. Rooted in skiffle, beat and 1950s rock 'n' roll, their sound incorporated
+      elements of classical music and traditional pop in innovative ways; the band also
+      explored music styles ranging from folk and Indian music to psychedelia and hard rock.
+      As pioneers in recording, songwriting and artistic presentation, the Beatles
+      revolutionised many aspects of the music industry and were often publicised as
+      leaders of the era's youth and sociocultural movements`,
+      author: "Wikipedia",
       model: "modelo2",
+    },
+    {
+      content: "help",
+      author: "The Beatles",
+      model: "modelo1",
     },
   ];
 }
--- src/app/components/thoughts/thought/thought.component.ts
+++ src/app/components/thoughts/thought/thought.component.ts
@@ -3,12 +3,19 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-thought',
   templateUrl: './thought.component.html',
-  styleUrls: ['./thought.component.css']
+  styleUrls: ['./thought.component.css'],
 })
 export class ThoughtComponent {
   @Input() thought = {
-    content: "it worked yesterday",
-    author: "Developer",
-    model: "modelo3",
+    content: 'it worked yesterday',
+    author: 'Developer',
+    model: 'modelo3',
   };
+
+  lenghtThought(): string {
+    if (this.thought.content.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
+  }
 }
